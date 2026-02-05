e38487a95074ecdef765fc890fbf04e7d3c001bc
Sammy-Mbugua <msammy542@gmail.com>
2026-02-05 15:02:27 +0300

    postgre_change

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -64,11 +64,11 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application '
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default='hosting_db'),
-        'USER': config('DB_USER', default='postgres'),
-        'PASSWORD': config('DB_PASSWORD', default='postgres'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'NAME': 'portfolio_db',
+        'USER': 'postgres',
+        'PASSWORD': 'your_postgres_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
